51a4605f16ca5e83c42e498404a97df5b1d242d2
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-03-03 16:38:00 -0500

    Add main function to arithmetic_formatter.py

--- arithmetic_formatter.py
+++ arithmetic_formatter.py
@@ -58,4 +58,9 @@ def arithmetic_arranger(problems, show_answers=False):
     return arranged_problems
 
 
-print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
+def main():
+    print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
+
+
+if __name__ == "__main__":
+    main()
